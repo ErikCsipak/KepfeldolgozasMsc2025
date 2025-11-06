90d03b771df321daf544708f5a3e01adbc4b2c49
Csipak Erik <80385882+ErikCsipak@users.noreply.github.com>
2025-11-06 12:23:38 +0100

    Refactored code

--- main.py
+++ main.py
@@ -1,47 +1,97 @@
 import cv2
 import numpy as np
+import snooker_colors as cr
 
 capture = cv2.VideoCapture("snooker_720p_sullivan.mp4")
-
-#red ball
-lower_red1 = np.array([0, 120, 70])
-upper_red1 = np.array([10, 255, 255])
-lower_red2 = np.array([170, 120, 70])
-upper_red2 = np.array([180, 255, 255])
 
 current_player = 1          # 1 or 2
 ball_to_pot = "Red"         # "Red" or "Color"
 previous_red_count = -1     # stores the red count from the last stable frame
+
+
+def normalize_frame(current_frame):
+    """
+    Normalizes one frame for better performance
+    :param current_frame: current frame of the given video
+    :return: a normalized frame in hsv color-space
+    """
+    f = cv2.resize(current_frame, (800, 450))
+    return cv2.cvtColor(f, cv2.COLOR_BGR2HSV)
+
+def detect_balls(h_f):
+    """
+    Detects the balls of the h_f frame and returns them in a list.
+    :param h_f: the hsv frame
+    :return: list of balls
+    """
+    return {
+        cr.RED: detect_balls_in_given_range(h_f, cr.lower_red, cr.upper_red),
+        cr.GREEN  : detect_balls_in_given_range(h_f, cr.lower_green, cr.upper_green),
+        cr.BLUE  : detect_balls_in_given_range(h_f, cr.lower_blue, cr.upper_blue),
+        cr.BROWN  : detect_balls_in_given_range(h_f, cr.lower_brown, cr.upper_brown),
+        cr.YELLOW  : detect_balls_in_given_range(h_f, cr.lower_yellow, cr.upper_yellow),
+        cr.PINK  : detect_balls_in_given_range(h_f, cr.lower_pink, cr.upper_pink),
+        cr.BLACK  : detect_balls_in_given_range(h_f, cr.lower_black, cr.upper_black),
+        cr.WHITE  : detect_balls_in_given_range(h_f, cr.lower_white, cr.upper_white)
+    }
+
+
+
+def detect_red_balls(hsv, l_r, u_r):
+    """
+    Detects red balls. Separate function needed because red balls are special case for HSV.
+    :param l_r: first part of the spectrum's lower red
+    :param u_r: first part of the spectrum's upper red
+    :param hsv: the hsv frame
+    :return: the detected red balls
+    """
+    mask1 = cv2.inRange(hsv, l_r, u_r)
+    mask2 = cv2.inRange(hsv, cr.lower_red2, cr.upper_red2)
+    mask = mask1 | mask2
+    # optimize for noise reduction
+    mask = cv2.GaussianBlur(mask, (9, 9), 2)
+    # detect balls
+    cv2.imshow("Red Mask", mask)
+    return cv2.HoughCircles(
+        mask,
+        cv2.HOUGH_GRADIENT, dp=1.2, minDist=20,
+        param1=50, param2=15, minRadius=6, maxRadius=15
+    )
+
+def detect_balls_in_given_range(hsv, lower, upper):
+    """
+    Detects balls in a given color-range.
+    :param hsv: the hsv frame
+    :param lower: the lower boundary of the given color
+    :param upper: the upper boundary of the given color
+    :return: the detected balls
+    """
+    if np.array_equal(lower, cr.lower_red):
+        return detect_red_balls(hsv, lower, upper)
+    else:
+        mask = cv2.inRange(hsv, lower, upper)
+        mask = cv2.GaussianBlur(mask, (9, 9), 2)
+
+        return cv2.HoughCircles(
+            mask,
+            cv2.HOUGH_GRADIENT, dp=1.2, minDist=20,
+            param1=50, param2=15, minRadius=6, maxRadius=15
+        )
 
 while capture.isOpened():
     ret, frame = capture.read()
     if not ret:
         break
 
-    # normalize frame (for speed)
-    frame = cv2.resize(frame, (800, 450))
-    
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
-    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
-    mask = mask1 | mask2
-
-    # optimize for noise reduction
-    mask = cv2.GaussianBlur(mask, (9, 9), 2)
-
-    # detect balls
-    circles = cv2.HoughCircles(
-        mask, 
-        cv2.HOUGH_GRADIENT, dp=1.2, minDist=20,
-        param1=50, param2=15, minRadius=6, maxRadius=15
-    )
+    hsv_frame = normalize_frame(frame)
+    balls = detect_balls(hsv_frame)
 
     current_red_count = 0
-    if circles is not None:
-        circles = np.uint16(np.around(circles))
-        current_red_count = len(circles[0])
-        for (x, y, r) in circles[0, :]:
+    if balls is not None:
+        print(balls)
+        balls = np.uint16(np.around(balls.get(cr.WHITE)))
+        current_red_count = len(balls[0])
+        for (x, y, r) in balls[0, :]:
             cv2.circle(frame, (x, y), r, (0, 255, 0), 2)    # circle around ball
             cv2.circle(frame, (x, y), 2, (255, 0, 0), 3)    # center dot
  
@@ -81,7 +131,6 @@ while capture.isOpened():
 
     # show result
     cv2.imshow("Snooker Red Ball Detection", frame)
-    cv2.imshow("Red Mask", mask)
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
---
+++ snooker_colors.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+RED = "red"
+GREEN = "green"
+BLUE = "blue"
+BROWN = "brown"
+PINK = "pink"
+BLACK = "black"
+WHITE = "white"
+YELLOW = "yellow"
+
+lower_yellow = np.array([20, 100, 100])
+upper_yellow = np.array([30, 255, 255])
+
+lower_green = np.array([45, 50, 50])
+upper_green = np.array([85, 255, 255])
+
+lower_brown = np.array([5, 70, 50])
+upper_brown = np.array([20, 200, 200])
+
+lower_blue = np.array([90, 80, 50])
+upper_blue = np.array([130, 255, 255])
+
+lower_pink = np.array([140, 70, 70])
+upper_pink = np.array([170, 255, 255])
+
+lower_black = np.array([0, 0, 0])
+upper_black = np.array([180, 255, 60])
+
+lower_white = np.array([0, 0, 200])
+upper_white = np.array([180, 60, 255])
+
+lower_red = np.array([0, 120, 70])
+upper_red = np.array([10, 255, 255])
+lower_red2 = np.array([170, 120, 70])
+upper_red2 = np.array([180, 255, 255])
